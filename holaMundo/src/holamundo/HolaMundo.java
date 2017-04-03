a8965156ce09d8de88dfe6f5e0eb7e80ad6d3447
speck85 <speck85@gmail.com>
2017-04-03 11:09:15 -0500

    Version de prueba Hola Mundo

--- holaMundo/src/holamundo/HolaMundo.java
+++ holaMundo/src/holamundo/HolaMundo.java
@@ -1,21 +1,14 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package holamundo;
-
 /**
  *
- * @author Gesfor Mexico
+ * @author Juan Carlos Garcia Bernal
  */
 public class HolaMundo {
-
     /**
-     * @param args the command line arguments
+     * @param args Argumentos de la linea de comandos
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+
         System.out.println("¡Hola Mundo!");
     }
     
